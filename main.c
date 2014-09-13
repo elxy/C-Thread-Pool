508ca0b6a5b2b2d6746ab7caecf2943b8443f963
elxy <elminxy@gmail.com>
2014-09-13 18:25:48 +0800

    Using variable i instead fixed a make it easier to debug.

--- main.c
+++ main.c
@@ -25,38 +25,35 @@
 
 /* Some arbitrary task 1 */
 void task1(){
-	printf("# Thread working: %u\n", (int)pthread_self());
-	printf("  Task 1 running..\n");
+	printf("# Thread %u work on task 1...\n\n", (int)pthread_self());
 }
 
 
 
 /* Some arbitrary task 2 */
 void task2(int a){
-	printf("# Thread working: %u\n", (int)pthread_self());
-	printf("  Task 2 running..\n");
-	printf("%d\n", a);
+	printf("# Thread %u work on task 2...\n", (int)pthread_self());
+	printf("  a = %d\n\n", a);
 }
 
 
 
 int main(){
 	int i;
-	
+
 	thpool_t* threadpool;             /* make a new thread pool structure     */
 	threadpool=thpool_init(4);        /* initialise it to 4 number of threads */
 
 
 	puts("Adding 20 tasks to threadpool");
-	int a=54;
 	for (i=0; i<10; i++){
-		thpool_add_work(threadpool, (void*)task1, NULL);
-		thpool_add_work(threadpool, (void*)task2, (void*)a);
+		thpool_add_work(threadpool, i, (void *)task1, NULL);
+		thpool_add_work(threadpool, i, (void *)task2, (void *)i);
 	};
 
 
     puts("Will kill threadpool");
 	thpool_destroy(threadpool);
-	
+
 	return 0;
 }
